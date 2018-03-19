bcbbe6ed412ce25ba4e4f6455651a31beb9edd23
Wale Orimogunje <waleorimogunje@wales-mbp.home>
2018-03-18 22:05:41 -0400

    Finish submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,25 @@ var wins = 0;
 var losses = 0;
 var guessleft = 10;
 var unanswered = [];
-var correctAns = [];
+var wrongAns = [];
 var letterguess = [];
+var docUnderScore = document.getElementsByClassName("underscore");
+var docrightAns = document.getElementsByClassName("rightGuess");
 
-    var computerGuess = rappers[Math.floor(Math.random() * rappers.length)];
-    // unanswered = rappers.splice(" ");
-    // console.log("rappers.splice: ",unanswered)
+var computerGuess = rappers[Math.floor(Math.random() * rappers.length)];
+// unanswered = rappers.splice(" ");
+// console.log("rappers.splice: ",unanswered)
 
-    var compGuessArray = computerGuess.toLowerCase().split("");
+var compGuessArray = computerGuess.toLowerCase().split("");
 
-    console.log("This is the array: " + compGuessArray);
-    for(var i = 0; i < compGuessArray.length; i++){
-        unanswered.push("_");
-    }
-        console.log("unanswered: ", unanswered);
+console.log("This is the array: " + compGuessArray);
+for (var i = 0; i < compGuessArray.length; i++) {
+    unanswered.push("_");
+}
+console.log("unanswered: ", unanswered);
 
 
-
+Winner();
 function evalGuess(person_guess) {
     if (guessleft => 1) {
         // for loops need to loop through arrays
@@ -29,15 +31,35 @@ function evalGuess(person_guess) {
         for (var i = 0; i < compGuessArray.length; i++) {
             // We are trying create a new array that consists of "_" for every letter of the word the computer chose
             if (person_guess === compGuessArray[i]) {
-                console.log('if person: ',person_guess, "if compG: ",compGuessArray)
+                console.log('if person: ', person_guess, "if compG: ", compGuessArray)
                 unanswered.splice(i, 1, person_guess);
-                console.log('correct: ',unanswered);
+                console.log('correct: ', unanswered);
+                wins++;
+
             }
+            
         }
         letterguess.push(person_guess);
-        console.log("Wrong ",letterguess);
-    } else {
+        docUnderScore[0].innerHTML = unanswered.join(' ');
+
+
+        
+    } 
+    else {
+        console.log("Wrong ", letterguess);
         alert("You ran out of chances")
+        losses++;
+    }
+
+} 
+ 
+
+function Winner(person_guess){
+    if(person_guess === compGuessArray[i]){
+        alert("You won!");
+    }
+    else{
+        alert("You lost");
     }
 }
 // console.log(unanswered.join(" "));
@@ -49,3 +71,5 @@ document.onkeyup = function (event) {
     console.log("Person Guess is: " + person_guess);
     evalGuess(person_guess.toLowerCase());
 }
+
+docUnderScore[0].innerHTML = unanswered.join(' ');
